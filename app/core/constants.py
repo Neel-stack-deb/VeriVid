f14f0bee65a6a7d92e520c11427f1313b1e8de42
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2026-07-10 21:33:11 +0530

    updated the video settings constants

--- app/core/constants.py
+++ app/core/constants.py
@@ -1,7 +1,19 @@
-UPLOAD_CHUNK_SIZE = 1024 * 1024
+UPLOAD_CHUNK_SIZE = 1024 * 1024  # 1 MB
+
 MAX_VIDEO_SIZE = 500 * 1024 * 1024  # 500 MB
-ALLOWED_VIDEO_TYPES = {
+
+ALLOWED_VIDEO_EXTENSIONS = {
+    ".mp4",
+    ".mov",
+    ".avi",
+    ".mkv",
+    ".webm",
+}
+
+ALLOWED_VIDEO_MIME_TYPES = {
     "video/mp4",
     "video/quicktime",
     "video/x-msvideo",
+    "video/x-matroska",
+    "video/webm",
 }
